818e69211d0d0e18a92349ea095dd5db6de4b0c1
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-20 05:47:09 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,11 +4,11 @@ class Dog
 
   attr_accessor :name, :breed, :age
 
-  @@all=[]
   def initialize(name, breed, age)
-    @name=name
-    @breed=breed
-    @age=age
+    @@all=[]
+    # @name=name
+    # @breed=breed
+    # @age=age
   end
 
 end
